9412fbccb51ce91a0829bac23b9748bd93502368
Jonas Arnold <jonas.arnold@live.de>
2018-12-07 16:54:11 +0100

    renamed file names

--- DmxRdmLib.cpp
+++ DmxRdmLib.cpp
@@ -14,7 +14,7 @@ You should have received a copy of the GNU General Public License along with thi
 If not, see http://www.gnu.org/licenses/
 */
 
-#include "espDMX.h"
+#include "DmxRdmLib.h"
 
 espDMX dmxA(0);
 espDMX dmxB(1);
